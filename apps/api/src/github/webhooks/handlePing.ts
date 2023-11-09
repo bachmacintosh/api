cd8ad2978dac8146eb38c6972dc3babf5635d7dd
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2023-11-09 14:07:41 -0500

    Adjust ping handling
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- apps/api/src/github/webhooks/handlePing.ts
+++ apps/api/src/github/webhooks/handlePing.ts
@@ -10,16 +10,23 @@ const handlePing: GitHubWebhookEventRunner<"ping"> = async (pingEvent, env, rest
   const embedFields: APIEmbedField[] = [{ name: "Name", value: pingEvent.hook.name }];
   if (pingEvent.hook.type === "Organization" && typeof pingEvent.organization !== "undefined") {
     embedFields.push({ name: "Organization", value: pingEvent.organization.login });
-  } else if (pingEvent.hook.type === "Repository" && typeof pingEvent.sender !== "undefined") {
-    embedFields.push({ name: "User", value: pingEvent.sender.login });
+  } else if (pingEvent.hook.type === "Repository" && typeof pingEvent.repository !== "undefined") {
+    if (typeof pingEvent.sender !== "undefined") {
+      embedFields.push({ name: "User", value: pingEvent.sender.login });
+      embedFields.push({ name: "Repository", value: pingEvent.repository.name });
+    } else if (typeof pingEvent.organization !== "undefined") {
+      embedFields.push({ name: "Organization", value: pingEvent.organization.login });
+      embedFields.push({ name: "Repository", value: pingEvent.repository.name });
+    }
   }
+  embedFields.push({ name: "Events", value: pingEvent.hook.events.join(", ") });
   await rest.post(Routes.channelMessages(env.DISCORD_CHANNEL_GITHUB), {
     body: {
       embeds: [
         resultEmbed(
           "success",
-          `New ${pingEvent.hook.type} Webhook Added`,
-          `Zen from GitHub: ${pingEvent.zen}`,
+          `New ${pingEvent.hook.type} Webhook Ping`,
+          "GitHub sent a Ping from a Webhook, usually when a new one was added.",
           embedFields,
         ),
       ],
