a9f438d2720d8ac9e6a0269cc421da4e62f714a1
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2023-11-09 15:37:51 -0500

    CS Alert Appeared in Branch/Closed by User
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- apps/api/src/github/webhooks/handleCodeScanningAlert.ts
+++ apps/api/src/github/webhooks/handleCodeScanningAlert.ts
@@ -1,4 +1,9 @@
-import { type GitHubWebhookEventRunner, type RESTPostAPIChannelMessageJSONBody, Routes } from "../../types";
+import {
+  type DiscordErrorLevel,
+  type GitHubWebhookEventRunner,
+  type RESTPostAPIChannelMessageJSONBody,
+  Routes,
+} from "../../types";
 import { HttpStatusCode } from "@bachmacintosh/api-types";
 import { StatusError } from "itty-router";
 import actionRow from "../../discord/components/actionRow";
@@ -9,17 +14,47 @@ import resultEmbed from "../../discord/embeds/resultEmbed";
 const handleCodeScanningAlert: GitHubWebhookEventRunner<"code_scanning_alert"> = async (event, env, rest) => {
   switch (event.action) {
     case "appeared_in_branch":
+      {
+        let alertLevel: DiscordErrorLevel = "warn";
+        switch (event.alert.rule.severity) {
+          case "error":
+            alertLevel = "error";
+            break;
+          case "none":
+          case "note":
+            alertLevel = "info";
+            break;
+          default:
+        }
+        await rest.post(Routes.channelMessages(env.DISCORD_CHANNEL_GITHUB), {
+          body: {
+            content: mentionUser(env.DISCORD_MENTION_ID),
+            embeds: [
+              resultEmbed(alertLevel, "Code Scanning Alert Appeared in Branch", event.repository.full_name, [
+                { name: "Severity", value: event.alert.rule.severity ?? "<unknown>", inline: true },
+                { name: "Rule", value: event.alert.rule.description, inline: true },
+              ]),
+            ],
+            components: [actionRow(linkButton("View Alert", event.alert.url))],
+          } satisfies RESTPostAPIChannelMessageJSONBody,
+        });
+      }
+      break;
+    case "closed_by_user":
       await rest.post(Routes.channelMessages(env.DISCORD_CHANNEL_GITHUB), {
         body: {
           content: mentionUser(env.DISCORD_MENTION_ID),
-          embeds: [resultEmbed("error", `Code Scanning Alert Appeared in ${event.repository.full_name}`)],
+          embeds: [
+            resultEmbed("info", "Code Scanning Alert Closed by User", event.sender.login, [
+              { name: "Repository", value: event.repository.full_name, inline: true },
+              { name: "Severity", value: event.alert.rule.severity ?? "<unknown>", inline: true },
+              { name: "Rule", value: event.alert.rule.description, inline: true },
+            ]),
+          ],
           components: [actionRow(linkButton("View Alert", event.alert.url))],
         } satisfies RESTPostAPIChannelMessageJSONBody,
       });
       break;
-    case "closed_by_user": {
-      throw new Error('Not implemented yet: "closed_by_user" case');
-    }
     case "created": {
       throw new Error('Not implemented yet: "created" case');
     }
