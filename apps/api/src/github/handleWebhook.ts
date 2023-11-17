a3ffba7c9f612d4357f570f5961fa709e058fc07
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2023-11-17 13:24:29 -0500

    Queue Webhook events instead
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- apps/api/src/github/handleWebhook.ts
+++ apps/api/src/github/handleWebhook.ts
@@ -13,16 +13,6 @@ import {
   type WebhookEventName,
 } from "../types";
 import { StatusError, json } from "itty-router";
-import {
-  handleCodeScanningAlert,
-  handleDependabotAlert,
-  handleDiscussion,
-  handleDiscussionComment,
-  handleIssueComment,
-  handleIssues,
-  handleMeta,
-  handlePing,
-} from "./webhooks";
 import { HttpStatusCode } from "@bachmacintosh/api-types";
 import getRest from "../discord/getRest";
 import mentionUser from "../discord/content/mentionUser";
@@ -38,50 +28,50 @@ export default async function handleWebhook(request: Request, env: Env): Promise
   switch (eventName) {
     case "code_scanning_alert":
       {
-        const event = await request.json<CodeScanningAlertEvent>();
-        await handleCodeScanningAlert(event, env, rest);
+        const payload = await request.json<CodeScanningAlertEvent>();
+        await env.QUEUE.send({ method: "processGitHubWebhook", params: { event: "code_scanning_alert", payload } });
       }
       break;
     case "dependabot_alert":
       {
-        const event = await request.json<DependabotAlertEvent>();
-        await handleDependabotAlert(event, env, rest);
+        const payload = await request.json<DependabotAlertEvent>();
+        await env.QUEUE.send({ method: "processGitHubWebhook", params: { event: "dependabot_alert", payload } });
       }
       break;
     case "discussion":
       {
-        const event = await request.json<DiscussionEvent>();
-        await handleDiscussion(event, env, rest);
+        const payload = await request.json<DiscussionEvent>();
+        await env.QUEUE.send({ method: "processGitHubWebhook", params: { event: "discussion", payload } });
       }
       break;
     case "discussion_comment":
       {
-        const event = await request.json<DiscussionCommentEvent>();
-        await handleDiscussionComment(event, env, rest);
+        const payload = await request.json<DiscussionCommentEvent>();
+        await env.QUEUE.send({ method: "processGitHubWebhook", params: { event: "discussion_comment", payload } });
       }
       break;
     case "issue_comment":
       {
-        const event = await request.json<IssueCommentEvent>();
-        await handleIssueComment(event, env, rest);
+        const payload = await request.json<IssueCommentEvent>();
+        await env.QUEUE.send({ method: "processGitHubWebhook", params: { event: "issue_comment", payload } });
       }
       break;
     case "issues":
       {
-        const event = await request.json<IssuesEvent>();
-        await handleIssues(event, env, rest);
+        const payload = await request.json<IssuesEvent>();
+        await env.QUEUE.send({ method: "processGitHubWebhook", params: { event: "issues", payload } });
       }
       break;
     case "meta":
       {
-        const event = await request.json<MetaEvent>();
-        await handleMeta(event, env, rest);
+        const payload = await request.json<MetaEvent>();
+        await env.QUEUE.send({ method: "processGitHubWebhook", params: { event: "meta", payload } });
       }
       break;
     case "ping":
       {
-        const event = await request.json<PingEvent>();
-        await handlePing(event, env, rest);
+        const payload = await request.json<PingEvent>();
+        await env.QUEUE.send({ method: "processGitHubWebhook", params: { event: "ping", payload } });
       }
       break;
     case "pull_request":
@@ -109,5 +99,5 @@ export default async function handleWebhook(request: Request, env: Env): Promise
       throw new StatusError(HttpStatusCode.NotImplemented, `GitHub Webhook Event ${eventHeader} Not Implemented`);
     }
   }
-  return json({ message: "Webhook successfully processed!" }, { status: HttpStatusCode.Ok });
+  return json({ message: "Webhook Payload Accepted" }, { status: HttpStatusCode.Accepted });
 }
