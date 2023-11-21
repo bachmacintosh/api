9c3ee78ecb62fdc7297082dba731b88d5c42bd0a
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2023-11-21 13:06:52 -0500

    Rolling Hourly Discord Channel Cleanup (#37)
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- apps/api/src/routes/cron/discord/cleanUpChannels.ts
+++ apps/api/src/routes/cron/discord/cleanUpChannels.ts
@@ -8,12 +8,15 @@ import type { REST } from "@discordjs/rest";
 import snowflakeToDate from "../../../discord/snowflakeToDate";
 
 export default async function cleanUpChannels(env: Env, rest: REST): Promise<void> {
+  const MESSAGE_BATCH_LIMIT = 100;
+  const ONE_DAY = 8.64e7;
+  const TWO_WEEKS = 1.21e9;
   const discordChannels = [env.DISCORD_CHANNEL_GITHUB, env.DISCORD_CHANNEL_SENSIBO, env.DISCORD_CHANNEL_STEAM] as const;
   for (const channel of discordChannels) {
     let hasMoreMessages = true;
     let before = "";
     while (hasMoreMessages) {
-      let queryString = `?limit=100`;
+      let queryString = `?limit=${MESSAGE_BATCH_LIMIT}`;
       if (before !== "") {
         queryString += `&before=${before}`;
       }
@@ -21,36 +24,43 @@ export default async function cleanUpChannels(env: Env, rest: REST): Promise<voi
       const messages = (await rest.get(
         `${Routes.channelMessages(channel)}${queryString}`,
       )) as RESTGetAPIChannelMessagesResult;
-      if (messages.length === 0) {
+      const hasMessagesOlderThanTwoWeeks = messages.some((message) => {
+        const currentTime = Date.now();
+        const messageTime = snowflakeToDate(message.id).getTime();
+        return currentTime - messageTime >= TWO_WEEKS;
+      });
+      if (messages.length < MESSAGE_BATCH_LIMIT || hasMessagesOlderThanTwoWeeks) {
         hasMoreMessages = false;
-      } else if (messages.length === 1) {
-        hasMoreMessages = false;
-        // eslint-disable-next-line no-await-in-loop -- Must be in order
-        await rest.delete(`${Routes.channelMessage(channel, messages[0].id)}`);
-      } else {
-        const messageIds = new Set<string>();
-        messages.sort((messageA, messageB) => {
-          const messageIdA = BigInt(messageA.id);
-          const messageIdB = BigInt(messageB.id);
-          const sortResult = messageIdA - messageIdB;
-          if (sortResult < Number.MIN_SAFE_INTEGER || sortResult > Number.MAX_SAFE_INTEGER) {
-            throw new RangeError("Returned ID range is too big to sort.");
-          }
-          return Number(sortResult);
-        });
-        for (const message of messages) {
-          const ONE_DAY = 8.64e7;
-          const currentTime = Date.now();
-          const messageTime = snowflakeToDate(message.id).getTime();
-          const shouldDelete = message.author.id === env.DISCORD_BOT_CLIENT_ID && currentTime - messageTime > ONE_DAY;
-          if (messages.indexOf(message) === 0) {
-            before = message.id;
-          }
-          if (shouldDelete) {
-            messageIds.add(message.id);
-          }
+      }
+      const messageIds = new Set<string>();
+      messages.sort((messageA, messageB) => {
+        const messageIdA = BigInt(messageA.id);
+        const messageIdB = BigInt(messageB.id);
+        const sortResult = messageIdA - messageIdB;
+        if (sortResult < Number.MIN_SAFE_INTEGER || sortResult > Number.MAX_SAFE_INTEGER) {
+          throw new RangeError("Returned ID range is too big to sort.");
         }
-        const messageIdArray = Array.from(messageIds);
+        return Number(sortResult);
+      });
+      for (const message of messages) {
+        const currentTime = Date.now();
+        const messageTime = snowflakeToDate(message.id).getTime();
+        const shouldDelete =
+          message.author.id === env.DISCORD_BOT_CLIENT_ID &&
+          currentTime - messageTime >= ONE_DAY &&
+          currentTime - messageTime < TWO_WEEKS;
+        if (messages.indexOf(message) === 0) {
+          before = message.id;
+        }
+        if (shouldDelete) {
+          messageIds.add(message.id);
+        }
+      }
+      const messageIdArray = Array.from(messageIds);
+      if (messageIdArray.length === 1) {
+        // eslint-disable-next-line no-await-in-loop -- Must be in order
+        await rest.delete(Routes.channelMessage(channel, messageIdArray[0]));
+      } else if (messageIdArray.length > 1) {
         // eslint-disable-next-line no-await-in-loop -- Must be in order
         await rest.post(Routes.channelBulkDelete(channel), {
           body: { messages: messageIdArray } satisfies RESTPostAPIChannelMessagesBulkDeleteJSONBody,
--- apps/api/src/routes/cron/handleScheduled.ts
+++ apps/api/src/routes/cron/handleScheduled.ts
@@ -5,7 +5,6 @@ import handleSensiboPods from "./sensibo/handleSensiboPods";
 import monitorSteamGame from "./steam/monitorSteamGame";
 
 export default async function handleScheduled(env: Env): Promise<void> {
-  const HOUR_08 = 8;
   const MINUTE_10 = 10;
   const dateOptions: Intl.DateTimeFormatOptions = {
     timeZone: "America/New_York",
@@ -18,17 +17,9 @@ export default async function handleScheduled(env: Env): Promise<void> {
 
   const rest = getRest(env);
 
-  switch (hour) {
-    case HOUR_08:
-      if (minute < MINUTE_10) {
-        await cleanUpChannels(env, rest);
-        await handleSensiboPods(env, rest, hour);
-      }
-      break;
-    default:
-      if (minute < MINUTE_10) {
-        await handleSensiboPods(env, rest, hour);
-      }
-      await monitorSteamGame(env, rest);
+  if (minute < MINUTE_10) {
+    await cleanUpChannels(env, rest);
+    await handleSensiboPods(env, rest, hour);
   }
+  await monitorSteamGame(env, rest);
 }
