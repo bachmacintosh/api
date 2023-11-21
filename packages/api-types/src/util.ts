a71f16d7c40543c82d1e61c51189d12bb270667a
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2023-11-21 03:51:29 -0500

    Types: Add Type Branding
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- packages/api-types/src/util.ts
+++ packages/api-types/src/util.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/naming-convention -- Type Branding Best Practice
+export type Brand<T, U extends string> = T & { __brand: U };
+
 export type RequireAtLeastOne<T extends object, Keys extends keyof T = keyof T> = {
   [K in Keys]-?: Partial<Pick<T, Exclude<Keys, K>>> & Required<Pick<T, K>>;
 }[Keys] &
