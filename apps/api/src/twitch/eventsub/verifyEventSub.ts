35d2e07c3b197b965411dd730869d8462a7e9477
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2023-11-21 15:32:10 -0500

    Try-Catch so we don't expose errors to attackers
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- apps/api/src/twitch/eventsub/verifyEventSub.ts
+++ apps/api/src/twitch/eventsub/verifyEventSub.ts
@@ -1,31 +1,36 @@
 import valueToUInt8Array from "../../util/valueToUInt8Array";
 
 export default async function verifyEventSub(request: Request, secret: string): Promise<boolean> {
-  const messageId = request.headers.get("Twitch-Eventsub-Message-Id");
-  const timestamp = request.headers.get("Twitch-Eventsub-Message-Timestamp");
-  const signatureHeader = request.headers.get("Twitch-Eventsub-Message-Signature");
+  try {
+    const messageId = request.headers.get("Twitch-Eventsub-Message-Id");
+    const timestamp = request.headers.get("Twitch-Eventsub-Message-Timestamp");
+    const signatureHeader = request.headers.get("Twitch-Eventsub-Message-Signature");
 
-  if (messageId === null || timestamp === null || signatureHeader === null) {
+    if (messageId === null || timestamp === null || signatureHeader === null) {
+      return false;
+    }
+
+    const signatureParts = signatureHeader.split("=");
+    const EXPECTED_LENGTH = 2;
+    if (signatureParts.length !== EXPECTED_LENGTH) {
+      return false;
+    }
+
+    const algorithm = { name: "HMAC", hash: { name: "SHA-256" } };
+    const secretBytes = valueToUInt8Array(secret);
+    const key = await crypto.subtle.importKey("raw", secretBytes, algorithm, false, ["sign", "verify"]);
+
+    const twitchSignatureString = signatureParts[1];
+    const twitchSignature = valueToUInt8Array(twitchSignatureString, "hex");
+
+    const body = await request.clone().text();
+    const messageText = messageId + timestamp + body;
+    const messageBytes = valueToUInt8Array(messageText);
+    const ourSignature = await crypto.subtle.sign(algorithm, key, messageBytes);
+
+    return crypto.subtle.timingSafeEqual(twitchSignature, ourSignature);
+  } catch (error) {
+    console.error(error);
     return false;
   }
-
-  const signatureParts = signatureHeader.split("=");
-  const EXPECTED_LENGTH = 2;
-  if (signatureParts.length !== EXPECTED_LENGTH) {
-    return false;
-  }
-
-  const algorithm = { name: "HMAC", hash: { name: "SHA-256" } };
-  const secretBytes = valueToUInt8Array(secret);
-  const key = await crypto.subtle.importKey("raw", secretBytes, algorithm, false, ["sign", "verify"]);
-
-  const twitchSignatureString = signatureParts[1];
-  const twitchSignature = valueToUInt8Array(twitchSignatureString, "hex");
-
-  const body = await request.clone().text();
-  const messageText = messageId + timestamp + body;
-  const messageBytes = valueToUInt8Array(messageText);
-  const ourSignature = await crypto.subtle.sign(algorithm, key, messageBytes);
-
-  return crypto.subtle.timingSafeEqual(twitchSignature, ourSignature);
 }
