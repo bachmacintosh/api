621674895874cfdb126b4015e08142cd554bafd2
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2023-11-29 14:50:52 -0500

    Use better phrasing for timeouts
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- apps/api/src/twitch/eventsub/subscriptions/handleChannelBan.ts
+++ apps/api/src/twitch/eventsub/subscriptions/handleChannelBan.ts
@@ -2,13 +2,11 @@ import type { EventSubWebhookNotificationHanlder } from "../../../types";
 import twitchEmbed from "../../../discord/embeds/twitchEmbed";
 
 const handleChannelBan: EventSubWebhookNotificationHanlder<"channel.ban"> = (event) => {
-  let moderatorOrBroadcaster = "Moderator";
-  if (event.moderator_user_id === event.broadcaster_user_id) {
-    moderatorOrBroadcaster = "Broadcaster";
-  }
-  const title = `User ${event.user_name} Banned by ${moderatorOrBroadcaster} ${event.moderator_user_name}`;
-  const description = `Banned for: ${event.reason} -- ${
-    event.is_permanent ? "Ban is Permanent" : `Ban Expires at ${event.ends_at}`
+  const bannedOrTimedOut = event.is_permanent ? "Banned" : "Timed Out";
+  const broadcasterOrModerator = event.moderator_user_id === event.broadcaster_user_id ? "Broadcaster" : "Moderator";
+  const title = `User ${event.user_name} ${bannedOrTimedOut} by ${broadcasterOrModerator} ${event.moderator_user_name}`;
+  const description = `${bannedOrTimedOut} for: ${event.reason} -- ${
+    event.is_permanent ? "Ban is Permanent" : `Timeout Expires at ${event.ends_at}`
   }`;
   return {
     needsMention: false,
