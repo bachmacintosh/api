04ea0605156e766d4bdb41412e330efef28a0b09
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2024-01-30 18:18:24 -0500

    Track Sheet Status with a type
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- apps/api/src/types/google/index.ts
+++ apps/api/src/types/google/index.ts
@@ -1,1 +1,11 @@
 export * from "./sheets";
+
+export interface SheetStatus {
+  doesExist: boolean;
+  hasData: boolean;
+  headerValues: string[];
+  neededColumns: number;
+  neededRows: number;
+  sheetId: number;
+  title: string;
+}
